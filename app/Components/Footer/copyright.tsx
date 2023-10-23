1838b2c8e45507b8dbe06348be20fc330c824c9e
anaghaweb <anaghawebdev@gmail.com>
2023-10-23 23:31:40 +0530

    footer component added

--- app/Components/Footer/copyright.tsx
+++ app/Components/Footer/copyright.tsx
@@ -5,8 +5,8 @@ function Copyright(props: any) {
   return (
     <Typography variant="body2" color="#B0B8C4" align="center" {...props}>
       {'Copyright © '}
-      <Link color='#B0B8C4' href="https://mui.com/">
-        Your Website
+      <Link color='#B0B8C4' href="#">
+        Our Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
